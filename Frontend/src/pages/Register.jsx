48f4da0437825ce4c40c67bdb20d3f3032fd109c
Sumit <skdudy546@gmail.com>
2026-06-14 21:42:27 +0530

    Auth Changes

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logoPng from "../assets/logo.png";
+import axios from "axios";
 import { GoogleLogin } from "@react-oauth/google";
 import { AnimatePresence, motion, useReducedMotion } from "framer-motion";
 import {
